5e77e49c8029a128d059f47dc6e3332e21cfd431
foxsis <va.four2016@gmail.com>
2022-10-14 19:27:21 +0300

    add cashe

---
+++ config/plugins.js
@@ -0,0 +1,24 @@
+module.exports = ({ env }) => ({
+    'import-export-entries': {
+      enabled: true,
+    },
+    "rest-cache": {
+        config: {
+          provider: {
+            name: "memory",
+            options: {
+              max: 32767,
+              maxAge: 600,
+            },
+          },
+          strategy: {
+            contentTypes: [
+              // list of Content-Types UID to cache
+              "api::product.product",
+
+            ],
+          },
+        },
+      },
+
+  });
