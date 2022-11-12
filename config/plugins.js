ac5fee50d022b90980cbd6ca9eeb45fdc0b5daae
foxsis <va.four2016@gmail.com>
2022-11-12 17:30:56 +0300

    dds3

--- config/plugins.js
+++ config/plugins.js
@@ -1,25 +1,4 @@
 module.exports = ({ env }) => ({
-    'import-export-entries': {
-      enabled: false,
-    },
-    "rest-cache": {
-      enabled: false, 
-        config: {
-          provider: {
-            name: "memory",
-            options: {
-              max: 32767,
-              maxAge: 600,
-            },
-          },
-          strategy: {
-            contentTypes: [
-              // list of Content-Types UID to cache
-              "api::product.product",
 
-            ],
-          },
-        },
-      },
 
   });
