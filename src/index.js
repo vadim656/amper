09bf5c400e50d925941fa6ccbfec22ada9d8ba19
foxsis <va.four2016@gmail.com>
2023-03-30 19:09:05 +0300

    fix 1.4

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-'use strict';
+'use strict'
+
 
 module.exports = {
   /**
@@ -7,7 +8,22 @@ module.exports = {
    *
    * This gives you an opportunity to extend code.
    */
-  register(/*{ strapi }*/) {},
+  register ({ strapi }) {
+    const extensionService = strapi.plugin('graphql').service('extension')
+    extensionService.use(({ nexus }) => ({
+      types: [
+        nexus.extendType({
+          type: 'UsersPermissionsMe',
+          definition (t) {
+            t.string('PHIO'),
+            t.string('Phone'),
+            t.string('Type')
+
+          }
+        })
+      ]
+    }))
+  },
 
   /**
    * An asynchronous bootstrap function that runs before
@@ -16,5 +32,5 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
-};
+  bootstrap (/*{ strapi }*/) {}
+}
